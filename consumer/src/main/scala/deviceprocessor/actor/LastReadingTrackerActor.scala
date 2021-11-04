3569f20bc567c17084777355503708102baaf131
Sergio Cano <canosergio90@gmail.com>
2021-11-03 22:35:45 -0300

    last reading tracker actor
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- consumer/src/main/scala/deviceprocessor/actor/LastReadingTrackerActor.scala
+++ consumer/src/main/scala/deviceprocessor/actor/LastReadingTrackerActor.scala
@@ -5,25 +5,40 @@ import java.util.UUID
 import akka.actor.typed.Behavior
 import deviceprocessor.domain.LastReading
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorRef
 
 object LastReadingTrackerActor {
 
   // protocol
   sealed trait Command
-  case class Process(deviceReading: DeviceReading) extends Command
-  case object GetLastValueReport                   extends Command
+  case class Process(deviceReading: DeviceReading)         extends Command
+  case class GetLastReads(replyTo: ActorRef[LastReadings]) extends Command
+
   // stream specific protocol
   case object StreamCompleted            extends Command
   case class StreamFailed(ex: Throwable) extends Command
+
+  case class LastReadings(result: List[LastReading])
 
   // behavior
   def apply(lastReadings: Map[UUID, LastReading] = Map()): Behavior[Command] =
     Behaviors.receive { (context, msg) =>
       msg match {
         case Process(deviceReading) =>
-        case GetLastValueReport     =>
-        case StreamCompleted        =>
-        case StreamFailed(ex)       =>
+          context.log.info("Adding device reading:")
+          val reading             = LastReading(deviceReading.deviceId, deviceReading.currentValue)
+          val updatedLastReadings = lastReadings + (deviceReading.deviceId -> reading)
+          LastReadingTrackerActor(updatedLastReadings)
+        case GetLastReads(replyTo) =>
+          context.log.info(s"Retrieving last readings: $lastReadings")
+          replyTo ! LastReadings(lastReadings.values.toList)
+          Behaviors.same
+        case StreamCompleted =>
+          context.log.info(s"Stream completed")
+          Behaviors.same
+        case StreamFailed(ex) =>
+          context.log.info(s"Retrieving last readings: $lastReadings")
+          Behaviors.same
       }
     }
 }
