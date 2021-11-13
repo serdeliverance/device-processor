d906672a4b633b5b3c65c8586b6eda481ee7e051
Sergio Cano <canosergio90@gmail.com>
2021-11-13 19:48:23 -0300

    av

--- commons/src/main/scala/deviceprocessor/queue/TimeBoundedDQueue.scala
+++ commons/src/main/scala/deviceprocessor/queue/TimeBoundedDQueue.scala
@@ -5,17 +5,17 @@ import deviceprocessor.queue.TimeBoundedDQueue.TimedEntry
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 
-class TimeBoundedDQueue[T] private (elements: List[TimedEntry[T]], timeWindow) extends DQueue[T] {
+case class TimeBoundedDQueue[T] private (elements: List[TimedEntry[T]], timeWindowInMinutes: Int) extends DQueue[T] {
 
   override def enqueue(t: T): DQueue[T] = {
-    val lowBoundTimeWindow = Instant.now().minus(timeWindow, ChronoUnit.MINUTES)
+    val lowBoundTimeWindow = Instant.now().minus(timeWindowInMinutes, ChronoUnit.MINUTES)
     val elementsInsideTimeWindow: List[TimedEntry[T]] =
-      elementsInsideTimeWindow.filter(elem => elem.enqueuedAt.isAfter(lowBoundTimeWindow))
-    new TimeBoundedDQueue[T](elementsInsideTimeWindow :+ TimedEntry(t), timeWindow)
+      elements.filter(elem => elem.enqueuedAt.isAfter(lowBoundTimeWindow))
+    this.copy(elements = elementsInsideTimeWindow :+ TimedEntry(t))
   }
 
   override def dequeue: (T, DQueue[T]) = elements match {
-    case ::(head, tail) => (head.entity, new TimeBoundedDQueue[T](tail, timeWindow))
+    case ::(head, tail) => (head.entity, new TimeBoundedDQueue[T](tail, timeWindowInMinutes))
     case Nil            => throw new RuntimeException("dequeue on empty queue")
   }
 
@@ -26,5 +26,5 @@ object TimeBoundedDQueue {
 
   case class TimedEntry[T](entity: T, enqueuedAt: Instant = Instant.now)
 
-  def apply[T](timeWindow: Int = 2): TimeBoundedDQueue[T] = new TimeBoundedDQueue[T](List.empty)
+  def apply[T](timeWindow: Int = 2): TimeBoundedDQueue[T] = new TimeBoundedDQueue[T](List.empty, timeWindow)
 }
--- commons/src/test/scala/deviceprocessor/queue/TimeBoundedDQueueSpec.scala
+++ commons/src/test/scala/deviceprocessor/queue/TimeBoundedDQueueSpec.scala
@@ -1,7 +1,56 @@
 package deviceprocessor.queue
 
+import deviceprocessor.domain.DeviceReading
+import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class TimeBoundedDQueueSpec extends AnyWordSpec {
-  // TODO unit test
+import java.time.Instant
+import java.util.UUID
+import scala.util.{Failure, Success, Try}
+
+class TimeBoundedDQueueSpec extends AnyWordSpec with Matchers {
+
+  private val readingOne = DeviceReading(UUID.randomUUID(), 10.0, "farenheit", Instant.now(), 1)
+  private val readingTwo = DeviceReading(UUID.randomUUID(), 20.0, "farenheit", Instant.now(), 1)
+
+  "enqueue reading correctly" in {
+    val queue  = TimeBoundedDQueue[DeviceReading]()
+    val result = queue.enqueue(readingOne).enqueue(readingTwo)
+
+    result.getAll mustBe List(readingOne, readingTwo)
+  }
+
+  "enqueue must retain elements inside time window" in {
+    // TODO
+  }
+
+  "dequeue correctly" in {
+    val readingThree = DeviceReading(UUID.randomUUID(), 30.0, "farenheit", Instant.now(), 1)
+    val queue        = TimeBoundedDQueue[DeviceReading]().enqueue(readingOne).enqueue(readingTwo).enqueue(readingThree)
+
+    val (dequeuedElement, updatedQueue) = queue.dequeue
+
+    dequeuedElement mustBe readingOne
+    updatedQueue.getAll mustBe List(readingTwo, readingThree)
+
+  }
+
+  "fail dequeing when queue is empty" in {
+    val emptyQueue = TimeBoundedDQueue[DeviceReading]()
+
+    val result = Try(emptyQueue.dequeue)
+
+    result match {
+      case Failure(exception) => succeed
+      case Success(_)         => fail()
+    }
+  }
+
+  "get all elements from empty queue must return empty result" in {
+    val emptyQueue = TimeBoundedDQueue[DeviceReading]()
+
+    val result = emptyQueue.getAll
+
+    result mustBe List.empty
+  }
 }
